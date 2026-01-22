e4df7fc81db088aca89a9bf939e7940bc276af54
andre <andredeomondes.dev@gmail.com>
2026-01-22 11:50:17 -0300

    feat: aula 19 - operadores de atribuição e unário

--- academy.devdojo.maratonajava.introducao/src/Aula04Operadores.java
+++ academy.devdojo.maratonajava.introducao/src/Aula04Operadores.java
@@ -1,3 +1,5 @@
+import java.sql.SQLOutput;
+
 public class Aula04Operadores {
     /**
      * <h1>Operadores</h1>
@@ -11,6 +13,7 @@ public class Aula04Operadores {
 
 
         // Aritméticos ---------------------------------------------------
+        System.out.println("----- Operadores Aritméticos -----");
         System.out.println("soma: " + (numero01 + numero02));
         System.out.println("subtração: " + (numero01 - numero02));
         System.out.println("multiplicação: " + (numero01 * numero02));
@@ -19,6 +22,7 @@ public class Aula04Operadores {
         System.out.println("módulo: " + (numero01 % numero02));
 
         // Relacionais ---------------------------------------------------------------
+        System.out.println("\n----- Operadores Relacionais -----");
         boolean isDezMaiorQueVinte = 10 > 20; // Operador maior que
         boolean isDezMenorQueVinte = 10 < 20; // Operador menor que
         boolean isDezIgualAVinte = 10 == 20; // Operador igual a
@@ -34,19 +38,38 @@ public class Aula04Operadores {
         float salario = 3500F;
 
         // Operador AND &&
+        System.out.println("\n----- Operador AND -----");
         boolean isDentroDaLeiMaiorQueTrinta = idade > 30 && salario >= 4612; // AND
         boolean isDentroDaLeiMenorQueTrinta = idade < 30 && salario >= 3381; // AND
         System.out.println("isDentroDaLeiMaiorQueTrinta: " + isDentroDaLeiMaiorQueTrinta);
         System.out.println("isDentroDaLeiMenorQueTrinta: " + isDentroDaLeiMenorQueTrinta);
 
         // Operador OR ||
+        System.out.println("\n----- Operador OR -----");
         double valorTotalContaCorrente = 200;
         double valorTotalContaPoupanca = 10000;
         float valorPlaystation = 5000F;
         boolean isPlaystationCompravel = valorTotalContaCorrente > valorPlaystation || valorTotalContaPoupanca > valorPlaystation;
         System.out.println("isPlaystationCompravel: " + isPlaystationCompravel);
 
+        // Operador = += -= *= /= %=
+        System.out.println("\n----- Operadores de Atribuição -----");
+        double bonus = 1800;
+        bonus += 1000; // bonus = bonus + 1000
+        bonus -= 1000; // bonus = bonus - 1000
+        bonus *= 2;    // bonus = bonus * 2
+        bonus /= 2;    // bonus = bonus / 2
+        bonus %= 2;    // bonus = bonus % 2
+        System.out.println("Valor do bônus: " + bonus);
 
+        // Operador Unário ++ --
+        int contador = 0;
+        contador +=1; // contador = contador + 1 incremento normal
+        contador++; // contador = contador + 1 incremento depois da variável
+        contador--; // contador = contador - 1 incremento depois da variável
+        ++contador; // contador = contador + 1 incremento antes da variável
+        --contador; // contador = contador - 1 decremento antes da variável
+        System.out.println("Contador: " + contador);
 
     }
 }
