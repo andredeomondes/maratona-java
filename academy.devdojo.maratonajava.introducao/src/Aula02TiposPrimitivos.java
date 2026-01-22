f180931ee8524755b27f41986b5faa3d0a0a2824
andre <andredeomondes.dev@gmail.com>
2026-01-22 07:55:32 -0300

    feat: aula 13 - tipos primitivos continuacao

--- academy.devdojo.maratonajava.introducao/src/Aula02TiposPrimitivos.java
+++ academy.devdojo.maratonajava.introducao/src/Aula02TiposPrimitivos.java
@@ -1,18 +1,22 @@
 public class Aula02TiposPrimitivos {
     public static void main(String[] args) {
-        int idade = (int) 10000000000000L; // 4 bte
+        // int, double , float , chat, byte , short, long , boolean
+        int idade = (int) 10000000000000L; // 4 bytes
         double salarioDouble = 2000; // 8 bytes
-        float salarioFloat = 2500.0f; //
+        float salarioFloat = (float) 2500.80; // 8 bytes
+        long numeroGrande = (long) 1000.45;
         byte idadeByte = -128; // 1 byte
         short idadeShort = 10; // 2 bytes
         boolean verdadeiro = true; // 1 bit
         boolean falso = false; // 1 bit
-        long numeroGrande = 100000L;
         char caractere = 87; // 2 bytes
+        String nome = "Um grande texto";
+
         System.out.println("A idade é " + idade);
         System.out.println(verdadeiro);
         System.out.println(falso);
         System.out.println(caractere);
+        System.out.println(salarioFloat);
 
     }
 }
