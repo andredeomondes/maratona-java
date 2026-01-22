84d541f41a000b44e7625a3185880028647f12bb
andre <andredeomondes.dev@gmail.com>
2026-01-22 11:36:45 -0300

    feat: aula 16 - operadores relacionais

--- academy.devdojo.maratonajava.introducao/src/Aula04Operadores.java
+++ academy.devdojo.maratonajava.introducao/src/Aula04Operadores.java
@@ -15,7 +15,19 @@ public class Aula04Operadores {
         System.out.println("subtração: " + (numero01 - numero02));
         System.out.println("multiplicação: " + (numero01 * numero02));
         System.out.println("divisão: " + (numero01 / (double) numero02));
+        // O operador módulo retorna o resto da divisão
         System.out.println("módulo: " + (numero01 % numero02));
+
+        //---------------------------------------------------------------
+        boolean isDezMaiorQueVinte = 10 > 20; // Operador maior que
+        boolean isDezMenorQueVinte = 10 < 20; // Operador menor que
+        boolean isDezIgualAVinte = 10 == 20; // Operador igual a
+        boolean isDezDiferenteDeVinte = 10 != 20; // Operador diferente de
+
+        System.out.println("isDezMaiorQueVinte: " + isDezMaiorQueVinte);
+        System.out.println("isDezMenorQueVinte: " + isDezMenorQueVinte);
+        System.out.println("isDezIgualAVinte: " + isDezIgualAVinte);
+        System.out.println("isDezDiferenteDeVinte: " + isDezDiferenteDeVinte);
 
         //---------------------------------------------------------------
 
